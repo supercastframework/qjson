11dded79ea10d6b43b3d5c64e56e5683b53a77a1
Sébastien Serre <sserre.bx@gmail.com>
2016-09-15 21:09:34 +0200

    initial

---
+++ Makefile
@@ -0,0 +1,12 @@
+
+.PHONY: all clean_all
+
+all:
+	make -C build all
+
+clean_all:
+	rm -rf build; rm Makefile
+
+%:
+	make -C build $@
+
---
+++ build/CMakeFiles/qjson.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/qjson.dir/qjson.cpp.o"
+  "libqjson.pdb"
+  "libqjson.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/qjson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
